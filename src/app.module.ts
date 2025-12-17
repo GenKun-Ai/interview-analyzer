ca49893a80433bde1a3479245ac9f94e575e0ba3
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-12-17 23:40:53 +0900

    feat: 프로젝트 기본설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,51 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const typeOrmAsyncOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [], // 테이블 관계
+    synchronize: true, // set 'false' in production
+    autoLoadEntities: true,
+    logging: true,
+  }),
+  inject: [ConfigService], // 의존성 -> process가 아니라 config-service로 가져옴
+};
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+        SECRET_KEY: Joi.string().required(),
+        ADMIN_USER: Joi.string().required(),
+        ADMIN_PASSWORD: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
