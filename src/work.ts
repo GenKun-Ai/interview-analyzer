9ea55e77977ef572713a09a2745e91a506e96c8d
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2026-01-18 22:11:48 +0900

    feat: 배포 서버용 worker 추가

---
+++ src/work.ts
@@ -0,0 +1,9 @@
+// src/worker.ts
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  await NestFactory.createApplicationContext(AppModule)
+  console.log('Worker started')
+}
+bootstrap()
